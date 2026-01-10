da601cd15703be77441e59525994c20df385fa09
Milos Stojkovic <milos@umbra.app>
2026-01-10 21:20:35 +0100

    fix(editor): preserve whitespace in web editors

--- muse/apps/web/src/main.tsx
+++ muse/apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { enableMapSet } from "immer";
 import { ConvexProvider } from "./providers";
 import App from "./App";
+import "prosemirror-view/style/prosemirror.css";
 import "./styles/globals.css";
 
 // Enable Immer MapSet plugin for Zustand stores that use Map/Set
--- muse/packages/editor-webview/src/components/EditorShell.tsx
+++ muse/packages/editor-webview/src/components/EditorShell.tsx
@@ -479,6 +479,7 @@ export function EditorShell({
             editable={!isLocked}
             showTitle={true}
             autoFocus
+            syncContentFromProps={false}
           />
         )}
 
